5914de219cbb55a258dd131e18c4ac3559eace9c
ericong18 <ericong18@gmail.com>
2019-10-11 18:49:40 -0700

    Start login page

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,11 +1,25 @@
 import React, { Component, Fragment } from 'react';
-import './Landing.module.css';
+import './Landing.css';
+import logo from '../../assets/imgs/roomster-logo.svg';
+import { Button } from 'antd';
+import { NavLink } from 'react-router-dom'
 
 class Landing extends Component {
   render() {
     return (
       <Fragment>
-        <div>This is landing</div>
+        <div className="container-fluid">
+          <div className="landing__logo-container">
+            <div className="row">
+              <img src={logo} className="landing__logo"/>
+            </div>
+            <div className="row justify-content-center">
+              <NavLink to="/login">
+                <Button type="primary">Get Started</Button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
       </Fragment>
     )
   }
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,11 +1,23 @@
 import React, { Component, Fragment } from 'react';
-import './Login.module.css';
+import './Login.css';
+import logo from '../../assets/imgs/roomster-logo.svg';
 
 class Login extends Component {
   render() {
     return (
       <Fragment>
-        <div>This is login</div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-5">
+              <div className="row justify-content-center">
+                <div className="login__logo-container">
+                  <img src={logo} className="login__logo"/>
+                </div>
+              </div>
+            </div>
+            <div className="col-7 login__right"></div>
+          </div>
+        </div>
       </Fragment>
     )
   }
