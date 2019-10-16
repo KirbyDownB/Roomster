c44e2e5a3fe1cd41288bb1020d55c56565a235e9
John Shin <jshin029@ucr.edu>
2019-10-16 15:44:27 -0700

    changed class names to match BEM standards

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Row, Container, Col } from 'react-bootstrap';
 import { Animated } from 'react-animated-css';
 import { Input, Icon, Button } from 'antd';
 import './Signup.css';
@@ -25,95 +24,95 @@ class Signup extends Component {
 
   render(){
     return(
-      <div className="signupBG">
+      <div className="signup__background">
         <Animated animationIn="fadeInLeft" isVisible={true}>
-          <Container fluid className="removePadding">
-            <Row className="removeMargin">
-              <Col xs lg="5" className="removePadding suLeft">
-                <div className="logoBox">
+          <div className="container-fluid signup__removePadding">
+            <div className="row signup__removeMargin">
+              <div className="col-6 signup__removePadding signup__suLeft">
+                <div className="signup__logoBox">
                   <img src={circleLogo}></img>
                 </div>
-              </Col>
-              <Col className="removePadding">
-                <div className="formBox">
-                  <h1 className="formTitle">Rooming made easy for you.</h1>
-                  <div className="inputBox">
+              </div>
+              <div className="col-6 signup__removePadding">
+                <div className="signup__formBox">
+                  <h1 className="signup__formTitle">Rooming made easy for you.</h1>
+                  <div className="signup__inputBox">
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Email *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Email *</p>
+                      <div className="signup__inputWrap">
                         <Input name="email" value={this.state.email} onChange={this.handleChange} placeholder="Username / Email" prefix={<Icon type="mail" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                     <div style={{width: '10%'}}></div>
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Profile</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Profile</p>
+                      <div className="signup__inputWrap">
                         <Input value={this.state.profile} onChange={this.handleChange} placeholder="Upload Picture" prefix={<Icon type="file-add" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
-                  <div className="inputBox2">
+                  <div className="signup__inputBox2">
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Password *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Password *</p>
+                      <div className="signup__inputWrap">
                         <Input name="password1" type="password" value={this.state.password1} onChange={this.handleChange} placeholder="Password" prefix={<Icon type="lock" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                     <div style={{width: '10%'}}></div>
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Password *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Password *</p>
+                      <div className="signup__inputWrap">
                         <Input name="password2" type="password" value={this.state.password2} onChange={this.handleChange} placeholder="Password" prefix={<Icon type="lock" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
-                  <div className="inputBox2">
+                  <div className="signup__inputBox2">
                     <div style={{width: "45%"}}>
-                      <p className="formBody">First Name *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">First Name *</p>
+                      <div className="signup__inputWrap">
                         <Input name="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name" prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                     <div style={{width: '10%'}}></div>
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Last Name *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Last Name *</p>
+                      <div className="signup__inputWrap">
                         <Input name="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder="Last Name" prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
-                  <div className="inputBox2">
+                  <div className="signup__inputBox2">
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Phone Number *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Phone Number *</p>
+                      <div className="signup__inputWrap">
                         <Input name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} placeholder="Phone Number" prefix={<Icon type="phone" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                     <div style={{width: '10%'}}></div>
                     <div style={{width: "45%"}}>
-                      <p className="formBody">Date of Birth *</p>
-                      <div className="inputWrap">
+                      <p className="signup__formBody">Date of Birth *</p>
+                      <div className="signup__inputWrap">
                         <Input name="dateOfBirth" value={this.state.dateOfBirth} onChange={this.handleChange} placeholder="Date of Birth" prefix={<Icon type="calendar" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
-                  <div className="inputBox2">
+                  <div className="signup__inputBox2">
                     <div style={{width: '100%'}}>
                       <Input name="address" value={this.state.address} onChange={this.handleChange} placeholder="Address" prefix={<Icon type="bank" style={{color: 'rgba(0, 0, 0)'}} />}/>
                     </div>
                   </div>
                 </div>
-                <div className="footer">
-                  <div className="buttonWrapper">
-                    <button className="prevButton">Previous</button>
+                <div className="signup__footer">
+                  <div className="signup__buttonWrapper">
+                    <button className="signup__prevButton">Previous</button>
                   </div>
-                  <div className="buttonWrapper2">
-                    <button className="nextButton">Next</button>
+                  <div className="signup__buttonWrapper2">
+                    <button className="signup__nextButton">Next</button>
                   </div>
                 </div>
-              </Col>
-            </Row>
-          </Container>
+              </div>
+            </div>
+          </div>
         </Animated>
       </div>
     )
