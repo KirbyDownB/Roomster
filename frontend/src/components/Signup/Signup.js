fa8477cb2e0ccab9851cf66f2f30ea98583bd220
John Shin <jshin029@ucr.edu>
2019-10-16 12:39:06 -0700

    fixed form and added footer

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -37,13 +37,14 @@ class Signup extends Component {
                 <div className="formBox">
                   <h1 className="formTitle">Rooming made easy for you.</h1>
                   <div className="inputBox">
-                    <div style={{width: "50%"}}>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Email *</p>
                       <div className="inputWrap">
-                        <Input value={this.state.email} onChange={this.handleChange} placeholder="Username / Email" prefix={<Icon type="mail" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="email" value={this.state.email} onChange={this.handleChange} placeholder="Username / Email" prefix={<Icon type="mail" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
-                    <div style={{width: "50%"}}>
+                    <div style={{width: '10%'}}></div>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Email *</p>
                       <div className="inputWrap">
                         <Input value={this.state.email} onChange={this.handleChange} placeholder="Username / Email" prefix={<Icon type="mail" style={{color: 'rgba(0, 0, 0)'}} />}/>
@@ -51,55 +52,62 @@ class Signup extends Component {
                     </div>
                   </div>
                   <div className="inputBox2">
-                    <div style={{width: "50%"}}>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Password *</p>
                       <div className="inputWrap">
-                        <Input type="password" value={this.state.password1} onChange={this.handleChange} placeholder="Password" prefix={<Icon type="lock" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="password1" type="password" value={this.state.password1} onChange={this.handleChange} placeholder="Password" prefix={<Icon type="lock" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
-                    <div style={{width: "50%"}}>
+                    <div style={{width: '10%'}}></div>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Password *</p>
                       <div className="inputWrap">
-                        <Input type="password" value={this.state.password2} onChange={this.handleChange} placeholder="Password" prefix={<Icon type="lock" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="password2" type="password" value={this.state.password2} onChange={this.handleChange} placeholder="Password" prefix={<Icon type="lock" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
                   <div className="inputBox2">
-                    <div style={{width: "50%"}}>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">First Name *</p>
                       <div className="inputWrap">
-                        <Input type="password" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name" prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name" prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
-                    <div style={{width: "50%"}}>
+                    <div style={{width: '10%'}}></div>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Last Name *</p>
                       <div className="inputWrap">
-                        <Input type="password" value={this.state.lastName} onChange={this.handleChange} placeholder="Last Name" prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder="Last Name" prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
                   <div className="inputBox2">
-                    <div style={{width: "50%"}}>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Phone Number *</p>
                       <div className="inputWrap">
-                        <Input type="password" value={this.state.phoneNumber} onChange={this.handleChange} placeholder="Phone Number" prefix={<Icon type="phone" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} placeholder="Phone Number" prefix={<Icon type="phone" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
-                    <div style={{width: "50%"}}>
+                    <div style={{width: '10%'}}></div>
+                    <div style={{width: "45%"}}>
                       <p className="formBody">Date of Birth *</p>
                       <div className="inputWrap">
-                        <Input type="password" value={this.state.dateOfBirth} onChange={this.handleChange} placeholder="Date of Birth" prefix={<Icon type="calendar" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                        <Input name="dateOfBirth" value={this.state.dateOfBirth} onChange={this.handleChange} placeholder="Date of Birth" prefix={<Icon type="calendar" style={{color: 'rgba(0, 0, 0)'}} />}/>
                       </div>
                     </div>
                   </div>
                   <div className="inputBox2">
-                    <div style={{width: '90%'}}>
-                      <Input type="password" value={this.state.address} onChange={this.handleChange} placeholder="Address" prefix={<Icon type="bank" style={{color: 'rgba(0, 0, 0)'}} />}/>
+                    <div style={{width: '100%'}}>
+                      <Input name="address" value={this.state.address} onChange={this.handleChange} placeholder="Address" prefix={<Icon type="bank" style={{color: 'rgba(0, 0, 0)'}} />}/>
                     </div>
                   </div>
-                  <div className="footer">
-                    <Button>Previous</Button>
-                    <Button>Next</Button>
+                </div>
+                <div className="footer">
+                  <div className="buttonWrapper">
+                    <button className="prevButton">Previous</button>
+                  </div>
+                  <div className="buttonWrapper2">
+                    <button className="nextButton">Next</button>
                   </div>
                 </div>
               </Col>
