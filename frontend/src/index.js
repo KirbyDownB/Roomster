66899960b8eaa9f8dba924a79ddb25e92a6428e5
John Shin <jshin029@ucr.edu>
2019-10-17 13:54:37 -0700

    added redux store and thunk to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const store = createStore( reducer, compose(applyMiddleware(thunk)))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
 serviceWorker.unregister();
---
+++ frontend/src/redux/reducer/authentication.js
@@ -0,0 +1,11 @@
+const initialState = {
+  user: {}
+}
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case: 'USER_LOGIN':
+      return {...state, user: action.payload};
+    default: return state;
+  }
+}
