84071380628e23e9a978e325e10aad5dbddc855f
John Shin <jshin029@ucr.edu>
2019-10-20 13:47:39 -0700

    added route for when token is passed back by backend (#14)

---
+++ frontend/src/components/PasswordReset/Reset.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Reset extends Component {
+  state = {
+
+  }
+
+  render(){
+    return(
+      <div>
+        this is reset
+      </div>
+    )
+  }
+}
+
+export default Reset;
