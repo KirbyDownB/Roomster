79a23742ac161f296b558c557f72354175ee4cda
John Shin <jshin029@ucr.edu>
2019-10-20 10:54:24 -0700

    added dispatch for refreshing token and included function call in app.js (#13)

--- frontend/src/redux/action.js
+++ frontend/src/redux/action.js
@@ -26,6 +26,32 @@ export const userLoginFetch = (username, password) => {
   }
 }
 
+export const tokenRefresh = () => {
+  return dispatch => {
+    if (localStorage.token) {
+      return fetch(`"${ BASE_URL}/api/validat_token"`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Token": localStorage.token
+        },
+        method: "GET",
+      })
+      .then(resp => resp.json())
+      .then(resp => {
+        if (resp.message === "Invalid"){
+          //response message could change this is just placeholder
+          localStorage.removeItem("token")
+        }
+        else {
+          localStorage.setItem("token", resp.token) //token time refreshed
+          //need to incorporate redux-persist in a future pr
+          //this can also be handled by backend 
+        }
+      })
+    }
+  }
+}
+
 const loginUser = (user) => ({
   type: 'LOGIN_USER',
   payload: user
