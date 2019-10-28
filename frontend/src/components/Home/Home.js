f1a6b18e0d837b5af0683346763b48d11ab7aa25
John Shin <jshin029@ucr.edu>
2019-10-27 21:24:41 -0700

    updated endpoints and set user in redux store

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Icon } from 'antd'
 import './Home.css';
 
 import Search from './Search/Search';
@@ -10,7 +11,16 @@ class Home extends Component {
       <div className="home__container">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-2">hi</div>
+            <div className="col-2 home__sideMenu">
+              <div className="home__innerMenu">
+                <Icon className="home__icon" type="user" />
+                <p className="home__menu_text">Profile</p>
+              </div>
+              <div className="home__innerMenu">
+                <Icon className="home__icon" type="menu" />
+                <p className="home__menu_text">Listings</p>
+              </div>
+            </div>
             <div className="col-10 home__main">
               <Search />
               <Listings />
