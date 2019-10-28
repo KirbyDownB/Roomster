1d1424b826cc86e581d2371001923d3c59892f80
Francisco Lagos Vilaboa <flagosvilaboa@gmail.com>
2019-10-27 19:31:24 -0700

    fix errors

--- backend/apis/update_profile.py
+++ backend/apis/update_profile.py
@@ -22,11 +22,11 @@ update_profile_data = api.model('update_profile_data', {
 })
 
 @api.route('/')
-class Listings(Resource):
+class update_profile(Resource):
     def get(self):
         return {"Message":"You sent a GET request"}
 
-    @api.expect(listings_data)
+    @api.expect(update_profile_data)
     def post(self):
         
         data = api.payload
@@ -38,18 +38,18 @@ class Listings(Resource):
         # in if
             # if email is not ""
                 # user_data.email = new_email
-                user_data = User(email=data.get('email'),\
-                    first_name=data.get('first_name'),\
-                    last_name=data.get('last_name'),\
-                    phone_number=data.get('phone_number'),\
-                    date_of_birth=data.get('date_of_birth'),\
-                    address=data.get('address'),\
-                    age=data.get('age'),\
-                    location_of_interest=data.get('location_of_interest'),\
-                    ethnicity=data.get('ethnicity'),\
-                    number_of_roommates=data.get('number_of_roommates'),\
-                    price_range_min=data.get('price_range_min'),\
-                    price_range_max=data.get('price_range_max'),\
+        user_data = User(email=data.get('email'),\
+            first_name=data.get('first_name'),\
+            last_name=data.get('last_name'),\
+            phone_number=data.get('phone_number'),\
+            date_of_birth=data.get('date_of_birth'),\
+            address=data.get('address'),\
+            age=data.get('age'),\
+            location_of_interest=data.get('location_of_interest'),\
+            ethnicity=data.get('ethnicity'),\
+            number_of_roommates=data.get('number_of_roommates'),\
+            price_range_min=data.get('price_range_min'),\
+            price_range_max=data.get('price_range_max'))
                 
         try:
             user_data.set_password(data.get('password'))
@@ -60,39 +60,39 @@ class Listings(Resource):
             print(e)
             return {"Message":"Something went wrong when updating your profile"}
 
-        if email is not ""
+        if email is not "":
             user_data.email = new_email
 
-        if first_name is not ""
+        if first_name is not "":
             user_data.first_name = new_first_name
 
-        if last_name is not ""
+        if last_name is not "":
             user_data.last_name =  new_last_name
 
-        if phone_number is not ""
+        if phone_number is not "":
             user_data.phone_number = new_phone_number
 
-        if date_of_birth is not ""
+        if date_of_birth is not "":
             user_data.date_of_birth = new_date_of_birth
 
-        if address is not ""
+        if address is not "":
             user_data.address = new_address
 
-        if age is not ""
+        if age is not "":
             user_data.age = new_age
 
-        if location_of_interest is not ""
+        if location_of_interest is not "":
             user_data.location_of_interest = new_location_of_interest
         
-        if ethnicity is not ""
+        if ethnicity is not "":
             user_data.ethnicity = new_ethnicity
 
-        if number_of_roommates is not ""
+        if number_of_roommates is not "":
             user_data.number_of_roommates = new_number_of_roommates
 
-        if price_range_min is not ""
+        if price_range_min is not "":
             user_data.price_range_min = new_price_range_min
 
-        if price_range_max is not ""
+        if price_range_max is not "":
             user_data.price_range_max = new_price_range_max
 
