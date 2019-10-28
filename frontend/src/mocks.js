890475c3e0ba895aea3ec1e0791de67195e14efb
Eric Ong <ericong18@gmail.com>
2019-10-27 21:51:26 -0700

    Task 26: Prepare to fetch from backend (#23)
    * Prepare to fetch from backend
    
    * Render appropriate page when clicking menu item

---
+++ frontend/src/mocks.js
@@ -0,0 +1,14 @@
+export const mockProfileInfo = {
+  email: 'test@fake.com',
+  firstName: 'Fake',
+  lastName: 'Name',
+  phoneNumber: '123456',
+  dob: '123',
+  address: '123',
+  age: '18',
+  location: 'test',
+  ethnicity: 'test',
+  numRoommates: 3,
+  priceLow: 2000,
+  priceHigh: 9000
+}
