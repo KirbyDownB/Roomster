c8fd9572a596f8692de9abe4c9d02bf03a35b2d7
ericong18 <ericong18@gmail.com>
2019-11-12 19:00:22 -0800

    Make signup one page

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,18 @@
+import { message } from 'antd';
+
 const BASE_URL = 'http://aa6dc42f.ngrok.io';
 const inputIconColor = { color: 'rgba(0, 0, 0)' };
+const PASSWORD_MATCH_ERROR = "Your passwords don't match!";
+const EMPTY_INPUT_ERROR = "You left an input field empty!";
+const PHONE_ERROR = "Your phone number is invalid.";
 
-export { BASE_URL, inputIconColor };
+const showErrorMessage = text => message.error(text);
+
+export {
+  BASE_URL,
+  inputIconColor,
+  showErrorMessage,
+  PASSWORD_MATCH_ERROR,
+  EMPTY_INPUT_ERROR,
+  PHONE_ERROR
+};
