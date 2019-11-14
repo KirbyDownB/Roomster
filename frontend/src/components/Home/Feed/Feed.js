c13041d94acc86b7d81fcdd8806d257b6f22a591
ericong18 <ericong18@gmail.com>
2019-11-13 22:19:48 -0800

    Set up post

--- frontend/src/components/Home/Feed/Feed.js
+++ frontend/src/components/Home/Feed/Feed.js
@@ -1,27 +1,97 @@
 import React, { Component } from 'react';
 import './Feed.css';
-import { Button, Icon, Popover } from 'antd';
-import Item from './Item/Item';
+import { Button, Icon, Popover, Input, Modal, Select, Form, Upload } from 'antd';
+import {
+  showSuccessMessage,
+  showErrorMessage,
+  postTags,
+  dummyRequest,
+  BASE_URL,
+  NEW_POST_SUCCESS,
+  NEW_POST_ERROR
+} from '../../../constants';
+
+const { Option } = Select;
+const { Item } = Form;
+const { TextArea } = Input;
+const { Dragger } = Upload;
 
 class Feed extends Component {
   state = {
-    isFilterOpen: false
+    isFilterOpen: false,
+    isNewPostModalOpen: false,
+    selectedTags: [],
+    isNewPostLoading: false,
+    images: []
   }
 
   showFilter = e => {
     e.preventDefault();
-    
     this.setState({ isFilterOpen: true });
   }
 
-  hideFilter = () => {
-    this.setState({
-      isFilterOpen: false
-    })
-  }
+  hideFilter = () => this.setState({ isFilterOpen: false });
 
   handleVisibleChange = isFilterOpen => {
     this.setState({ isFilterOpen });
+  }
+
+  handleNewPostClick = e => {
+    e.preventDefault();
+    this.setState({ isNewPostModalOpen: true });
+  }
+
+  closeNewPostModal = () => this.setState({ isNewPostModalOpen: false });
+
+  handleNewPostSubmit = e => {
+    e.preventDefault();
+    this.setState({ isNewPostLoading: true });
+
+    const content = e.target.content.value;
+    const tags = this.state.selectedTags;
+    const images = this.state.images;
+
+    const data = { content, tags, images };
+
+    fetch(`${BASE_URL}/api/posting/add/`, {
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': this.props.match.params.token
+      },
+      method: "POST",
+      body: JSON.stringify({ ...data }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Got response after submitting post", data);
+        this.setState({
+          isNewPostLoading: false,
+          isNewPostModalOpen: false
+        });
+        showSuccessMessage(NEW_POST_SUCCESS);
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({
+          isNewPostLoading: false,
+          isNewPostModalOpen: false
+        });
+        showErrorMessage(NEW_POST_ERROR);
+      })
+  }
+
+  handleTagSelect = selectedTags => this.setState({ selectedTags });
+
+  handlePostImageUpload = info => {
+    const { file: { status, name }, fileList } = info;
+    const images = fileList.map(({ originFileObj }) => originFileObj);
+
+    if (status === 'done') {
+      showSuccessMessage(`${name} has successfully been uploaded!`);
+      this.setState({ images });
+    } else if (status === 'error') {
+      showErrorMessage(`${name} couldn't be uploaded.`);
+    }
   }
 
   render() {
@@ -29,9 +99,19 @@ class Feed extends Component {
       <div className="feed__container">
         <div className="container-fluid">
           <div className="row justify-content-center">
-            <div className="col-10">
+            <div className="col-2">
               <h2 className="feed__title">Feed</h2>
             </div>
+            <div className="col-10">
+              <Input
+                prefix={<Icon type="search"/>}
+                placeholder="Search for posts"
+                className="feed__search-input"
+                onPressEnter={this.handleSearch}
+              />
+            </div>
+          </div>
+          <div className="row">
             <div className="col-2">
               <Popover
                 content={<a onClick={this.hideFilter}>Close</a>}
@@ -44,14 +124,81 @@ class Feed extends Component {
                   type="default"
                   className="feed__filter--button"
                   onClick={this.showFilter}
-                  size="large"
                 >
                   <Icon type="control" />
                   Filter
                 </Button>
               </Popover>
             </div>
+            <div className="col-2">
+              <Button
+                type="primary"
+                className="feed__newPost--button"
+                onClick={this.handleNewPostClick}
+              >
+                <Icon type="plus-circle" theme="filled"/>
+                New Post
+              </Button>
+            </div>
           </div>
+          <Modal
+            visible={this.state.isNewPostModalOpen}
+            onOk={this.handleOk}
+            onCancel={this.closeNewPostModal}
+            footer={null}
+            centered
+          >
+            <Form onSubmit={this.handleNewPostSubmit}>
+              <div className="newPost__title">New Post</div>
+              <Item>
+                <div className="newPost__caption">Tags</div>
+                <Select
+                  mode="tags"
+                  style={{ width: '100%' }}
+                  placeholder="Tags Mode"
+                  onChange={this.handleTagSelect}
+                >
+                  {postTags.map(postTag => <Option key={postTag}>{postTag}</Option>)}
+                </Select>
+              </Item>
+              <Item>
+                <div className="newPost__caption">Post</div>
+                <TextArea
+                  rows={4}
+                  name="content"
+                  placeholder="Type your post here..."
+                  autoSize={{ maxRows: 5 }}
+                />
+              </Item>
+              <Item>
+                <div className="newPost__caption">Upload Images</div>
+                <Dragger
+                  name="file"
+                  onChange={this.handlePostImageUpload}
+                  customRequest={dummyRequest}
+                  listType="picture"
+                  accept=".jpg, .JPG, .png, .PNG"
+                >
+                  <p className="ant-upload-drag-icon">
+                    <Icon type="inbox" />
+                  </p>
+                  <p className="newPost__upload--description">
+                    Drag and drop a maximum of 5 images.
+                  </p>
+                </Dragger>
+              </Item>
+              <Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="newPost__submit"
+                  loading={this.state.isNewPostLoading}
+                >
+                  SUBMIT
+                </Button>
+              </Item>
+            </Form>
+          </Modal>
         </div>
       </div>
     )
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -8,9 +8,12 @@ const NO_IMAGE_ERROR = "You forgot to upload an image!";
 const PHONE_ERROR = "Your phone number is invalid.";
 const EMAIL_RESET_MESSAGE = "A password reset link has been sent to your email.";
 const SIGNUP_SUCCESS= "You've successfully signed up! You'll be redirected to the login page in 3 seconds";
+const NEW_POST_SUCCESS = "Your post has successfully been submitted!";
+const NEW_POST_ERROR = "Sorry, we weren't able to submit your post!";
 const SIGNUP_ERROR = "An issue occurred when trying to sign you up.";
 const LOGIN_ERROR = "Sorry, we had trouble logging you in!";
 const GENERAL_ERROR = "Something went wrong!";
+const NUM_IMAGE_UPLOAD_ERROR = "Please upload no more than 5 images!";
 const durations = ["0 - 6 months", "6 months - 1 year", "1 year +"];
 const ethnicities = [
   "American Indian or Alaska Native",
@@ -20,6 +23,28 @@ const ethnicities = [
   "Native Hawaiian or Other Pacific Islander",
   "White"
 ];
+const postTags = [
+  "outdoors",
+  "insipirational",
+  "fun",
+  "new beginnings",
+  "happy",
+  "searching"
+]
+
+// possible categories: listing, meetup, 
+
+const mockPosts = [
+  {
+    name: "Eric Ong",
+    date: "November 13, 2019",
+    content: "Post content",
+    tags: ["tag1", "tag2"],
+    images: ["url1", "url2"],
+    numLikes: 1,
+    numDislikes: 1,
+  }
+]
 
 const showErrorMessage = text => message.error(text);
 const showSuccessMessage = text => message.success(text);
@@ -38,10 +63,15 @@ export {
   LOGIN_ERROR,
   EMAIL_RESET_MESSAGE,
   GENERAL_ERROR,
+  NUM_IMAGE_UPLOAD_ERROR,
   SIGNUP_SUCCESS,
   SIGNUP_ERROR,
+  NEW_POST_SUCCESS,
+  NEW_POST_ERROR,
   durations,
   ethnicities,
   dummyRequest,
-  loginRedirect
+  loginRedirect,
+  mockPosts,
+  postTags
 };
