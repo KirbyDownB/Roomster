2ba37bb4f63fd840a0913e56acbed6d4fa9d18d3
John Shin <jshin029@ucr.edu>
2019-11-23 16:11:04 -0800

    updated cards

--- frontend/src/components/Home/Groups/Groups.js
+++ frontend/src/components/Home/Groups/Groups.js
@@ -1,68 +1,116 @@
 import React, { Component } from 'react';
 import GroupCards from './GroupCards/GroupCards';
 import Reply from './Reply/Reply';
-import { Popover, Icon, Popconfirm, Input } from 'antd';
+import { Popover, Icon, Popconfirm, Input, Button, Modal } from 'antd';
 import './Groups.css';
 
 const { Search } = Input;
+const groupImg = require('../../../assets/imgs/people-2.svg')
 const eric = require('../../../assets/eric.jpg')
 const aditya = require('../../../assets/aditya.jpg')
 
 class Groups extends Component {
 
-  formatTitles = () => {
+  state = {
+    visible: false,
+    groupList: []
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleOk = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  groupList = () => {
+    //fetch()
+    // let fake = [
+    //   {
+    //     name: "Aditya Acharya",
+    //     pf_pic: aditya,
+    //   },
+    //   {
+    //     name: "Eric Ong",
+    //     pf_pic: eric
+    //   }
+    // ]
+    //
+    // this.setState({
+    //   groupList: fake
+    // })
+  }
+
+  modalContent = () => {
     //add fetching of groups here
     return(
-      <div className="groupcards__title-format">
-        <div className="groupcards__title-container">
-          <img src={aditya} className="groupcards__title-image"></img>
-          <h4 className="groupcards__title-list">Cheese its</h4>
+      <div>
+        <div className="groups__sections">
+          <h6>Settings</h6>
+          <div className="groups__people-container">
+            <Popconfirm
+              placement="right"
+              title="Are you sure?"
+              icon={<Icon type="question-circle-o" style={{ color: 'red' }} />}
+              trigger="click"
+            >
+              <Icon className="groups__icon-button" type="exclamation-circle" />
+            </Popconfirm>
+            <p className="groups__people-text">Delete Group</p>
+          </div>
         </div>
-        <div className="groupcards__title-container">
-          <img src={aditya} className="groupcards__title-image"></img>
-          <h4 className="groupcards__title-list">Crackers</h4>
-        </div>
+        <div className="groups__sections">
+        <h6>Members</h6>
+          <div className="groups__people-container">
+            <Icon className="groups__icon-button" type="plus" />
+            <p className="groups__people-text">Add People</p>
+          </div>
+          {this.state.groupList.map(({ name, pf_pic }, index) => {
+            return(
+              <div className="groups__user-container">
+                <img className="groups__user-img" src={pf_pic}></img>
+                <p className="groups__user-name">{name}</p>
+              </div>
+            )
+          })
+          }
+          </div>
       </div>
     )
   }
 
+  componentDidMount(){
+    this.groupList();
+  }
+
   render(){
     return(
+      <div>
+      {this.state.groupList.length > 0 ?
       <div className="container-fluid groups__container">
       <div className="row">
         <div className="col">
           <div className="groupcards__header">
-            <Popover
-              placement="bottomLeft"
-              content={this.formatTitles()}
-              trigger="click"
-            >
-              <h2 className="groupcards__header-text">KirbyDownB</h2>
-            </Popover>
+              <h2 className="groupcards__header-text" onClick={this.showModal}>KirbyDownB</h2>
+              <Modal
+                title="Group Settings"
+                visible={this.state.visible}
+                onOk={this.handleOk}
+                onCancel={this.handleOk}
+                footer={[
+                  <Button key="back" onClick={this.handleOk}>
+                    Return
+                  </Button>
+                ]}
+              >
+                {this.modalContent()}
+              </Modal>
             <div className="groupcards__icon-container">
-              <Popover
-                placement="right"
-                title="Add Members"
-                content={
-                  <div>
-                  <Search
-                     placeholder="input username"
-                     onSearch={value => this.handleSearch(value)}
-                   />
-                  </div>
-                }
-                trigger="click"
-              >
-                <Icon className="groupcards__icon-button" type="plus-circle" />
-              </Popover>
-              <Popconfirm
-                placement="right"
-                title="Delete this group?"
-                icon={<Icon type="question-circle-o" style={{ color: 'red' }} />}
-                trigger="click"
-              >
-                <Icon className="groupcards__icon-button" type="minus-circle" />
-              </Popconfirm>
             </div>
           </div>
         </div>
@@ -73,11 +121,25 @@ class Groups extends Component {
               <div className="groups__chat-box">
                 <GroupCards />
                 <Reply />
+                <Reply />
+                <GroupCards />
               </div>
             </div>
           </div>
         </div>
+      </div>:
+      <div className="groups__empty-container">
+        <div className="groups__empty-img-container">
+          <div className="groups__empty-title-container">
+            <h2 className="groups__empty-title">You are currently not part of a group</h2>
+            <Button type="primary" shape="circle" icon="search" />
+          </div>
+          <img className="groups__empty-img" src={groupImg}>
+          </img>
+        </div>
       </div>
+    }
+    </div>
     )
   }
 }
