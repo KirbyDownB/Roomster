b9ecf91ce606d280576a546c4461836b68f05345
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2019-11-25 11:46:18 -0800

    More group stuff (#65)
    * socket and group stuff
    
    * group endpoint finished

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from apis import api,app
+from apis import api,app, socketio
+from flask_socketio import send,emit
 
 api.init_app(app)
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + str(message))
+    send(message, broadcast=True)
+    emit('my response', message)
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+   
